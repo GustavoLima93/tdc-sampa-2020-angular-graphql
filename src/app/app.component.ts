09f24ff00d8b3f9c01faa5259d3ea82f0f334fb2
Gustavo Lima <gholiveira>
2020-08-26 12:38:20 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Apollo } from 'apollo-angular';
+
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tdc-graphql';
+
+  public pokemons = [];
+
+  private page = 1;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .query({
+        query: gql`
+          query {
+            pokemons {
+              id
+              nome
+              img
+              numeroDex
+            }
+          }
+        `,
+      })
+      .subscribe((resp: any) => {
+        this.pokemons = resp.data.pokemons;
+      });
+  }
+
+  carregarMais() {
+    this.page += 1;
+
+    this.apollo
+      .query({
+        query: gql`
+          query {
+            pokemons(page: ${this.page}, size: ${10}) {
+              id
+              nome
+              img
+              numeroDex
+            }
+          }
+        `,
+      })
+      .subscribe((resp: any) => {
+        this.pokemons = [...this.pokemons, ...resp.data.pokemons];
+        console.log(this.pokemons);
+      });
+  }
 }
